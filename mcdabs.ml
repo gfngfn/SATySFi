926077a0b2999f5e306f5095e2af4d62dc94cb75
gfngfn <bd.gfngfn@gmail.com>
2015-03-22 22:20:09 +0900

    fixed EmptyAbsBlock but ListBySep still has bugs

--- mcdabs.ml
+++ mcdabs.ml
@@ -25,55 +25,67 @@ end = struct
   let rec concrete_to_abstract conctr =
     match conctr with
       NonTerminal(Total, [tr; Terminal(END_OF_INPUT)]) -> (
-        	print_string "#Total" ; print_newline () ;
-      	  concrete_to_abstract tr
+          print_string "#Total" ; print_newline () ;
+          concrete_to_abstract tr
         )
-    | NonTerminal(Block, []) -> (
-        	print_string "#Block []" ; print_newline () ;
-    	    EmptyAbsBlock
-    	  )
-    | NonTerminal(Block, stc :: blk) -> (
-        	print_string "#Block stc :: blk" ; print_newline () ;
-      	  AbsBlock(concrete_to_abstract stc, concrete_to_abstract (NonTerminal(Block, blk)))
-      	)
+    | NonTerminal(Block, chdrn) -> (
+          match chdrn with
+            [] -> (
+                print_string "#Block []" ; print_newline () ;
+                EmptyAbsBlock
+              )
+          | [NonTerminal(Block, [])] -> (
+                print_string "#Block [*]" ; print_newline () ;
+                EmptyAbsBlock
+              )
+          | stc :: blk -> (
+                print_string "#Block stc :: blk" ; print_newline () ;
+                print_tree_node stc ; print_newline () ;
+                print_input blk ; print_newline () ;
+                AbsBlock(concrete_to_abstract stc, concrete_to_abstract (NonTerminal(Block, blk)))
+              )
+        )
     | NonTerminal(Group, [Terminal(BGRP); lstbysp; Terminal(EGRP)]) -> (
-        	print_string "#Group" ; print_newline () ;
-    	    concrete_to_abstract lstbysp
-    	  )
-    | NonTerminal(ListBySep, (NonTerminal(Block, blk)) :: lstbysp) -> (
+          print_string "#Group" ; print_newline () ;
+          print_tree_node lstbysp ; print_newline () ;
+          concrete_to_abstract lstbysp
+        )
+    | NonTerminal(ListBySep, tr :: lstbysp) -> (
           match lstbysp with
             [] -> (
-              	print_string "#ListBySep []" ; print_newline () ;
-            	  concrete_to_abstract (NonTerminal(Block, blk))
-            	)
+                print_string "#ListBySep []" ; print_newline () ;
+                print_tree_node tr ; print_newline () ;
+                concrete_to_abstract tr
+              )
           | _ -> (
-              	print_string "#ListBySep _" ; print_newline () ;
-          	    Separated(concrete_to_abstract (NonTerminal(Block, blk)), concrete_to_abstract (NonTerminal(Block, lstbysp)))
-          	  )
+                print_string "#ListBySep _" ; print_newline () ;
+                print_tree_node tr ; print_newline () ;
+                Separated(concrete_to_abstract tr, concrete_to_abstract (NonTerminal(Block, lstbysp)))
+              )
         )
     | NonTerminal(Sentence, chdrn) -> (
         match chdrn with
         (* S -> [char] *)
-      	  [Terminal(CHAR(c))] -> (
-            	print_string "#Sentence CHAR" ; print_newline () ;
-      	  	  Output(c)
-      	  	)
+          [Terminal(CHAR(c))] -> (
+              print_string "#Sentence CHAR" ; print_newline () ;
+              Output(c)
+            )
         (* S -> [var] *)
         | [Terminal(VAR(v)); Terminal(END)] -> (
-            	print_string "#Sentence VAR" ; print_newline () ;
-        	    ContentOf(v)
-        	  )
+              print_string "#Sentence VAR" ; print_newline () ;
+              ContentOf(v)
+            )
         (* S -> [pop] [var] [var] G G *)
         | [Terminal(POP); Terminal(VAR(u)); Terminal(VAR(v)); grp1; grp2]
             -> (
-            	print_string "#Sentence POP" ; print_newline () ;
-            	Pop(u, v, concrete_to_abstract grp1, concrete_to_abstract grp2)
+              print_string "#Sentence POP" ; print_newline () ;
+              Pop(u, v, concrete_to_abstract grp1, concrete_to_abstract grp2)
             )
         (* S -> [macro] [ctrlseq] A G *)
         | [Terminal(MACRO); Terminal(CTRLSEQ(f)); args; grp1]
             -> (
-            	print_string "#Sentence MACRO" ; print_newline () ;
-            	Macro(f, make_args_list args, concrete_to_abstract grp1, EmptyAbsBlock)
+              print_string "#Sentence MACRO" ; print_newline () ;
+              Macro(f, make_args_list args, concrete_to_abstract grp1, EmptyAbsBlock)
             )
         (* S -> [macrowid] [ctrlseq] A G G *)
   (*    | [Terminal(MACROWID); Terminal(CTRLSEQ(f)); args; grp1; grp2]
@@ -81,49 +93,57 @@ end = struct
         (* S -> [ctrlseq] [end] *)
         | [Terminal(CTRLSEQ(f)); Terminal(END)]
             -> (
-            	print_string "#Sentence CTRLSEQ1" ; print_newline () ;
-            	Apply(f, NoID, [])
+              print_string "#Sentence CTRLSEQ1" ; print_newline () ;
+              Apply(f, NoID, [])
             )
         (* S -> [ctrlseq] [id] [end] *)
         | [Terminal(CTRLSEQ(f)); Terminal(ID(i)); Terminal(END)]
             -> (
-            	print_string "#Sentence CTRLSEQ2" ; print_newline () ;
-            	Apply(f, RealID(i), [])
+              print_string "#Sentence CTRLSEQ2" ; print_newline () ;
+              Apply(f, RealID(i), [])
             )
         (* S -> [ctrlseq] G P *)
         | [Terminal(CTRLSEQ(f)); grp; prms]
             -> (
-            	print_string "#Sentence CTRLSEQ3" ; print_newline () ;
-            	Apply(f, NoID, (concrete_to_abstract grp) :: (make_params_list prms))
+              print_string "#Sentence CTRLSEQ3" ; print_newline () ;
+              Apply(f, NoID, (concrete_to_abstract grp) :: (make_params_list prms))
             )
         (* S -> [ctrlseq] [id] G P *)
         | [Terminal(CTRLSEQ(f)); Terminal(ID(i)); grp; prms]
             -> (
-            	print_string "#Sentence CTRLSEQ4" ; print_newline () ;
-            	Apply(f, RealID(i), (concrete_to_abstract grp) :: (make_params_list prms))
+              print_string "#Sentence CTRLSEQ4" ; print_newline () ;
+              Apply(f, RealID(i), (concrete_to_abstract grp) :: (make_params_list prms))
             )
         | _ -> (report_error "illegal child of sentence" ; Invalid)
       )
-    | _ -> (report_error "illegal concrete tree" ; Invalid)
+    | Terminal(END_OF_INPUT) -> (
+    	    print_string "#END_OF_INPUT" ; print_newline () ;
+    	    EmptyAbsBlock
+        )
+    | _ -> (
+    	  report_error "illegal concrete tree" ;
+    	  print_tree_node conctr ; print_newline () ;
+        Invalid
+      )
 
     (* tree -> var_name list *)
     and make_args_list args =
       match args with
         NonTerminal(Args, []) -> (
-          	print_string "#Args []" ; print_newline () ;
-        	  []
+            print_string "#Args []" ; print_newline () ;
+            []
           )
       | NonTerminal(Args, Terminal(VAR(v)) :: argssub)
           -> (
-          	print_string "#Args v :: argssub" ; print_newline () ;
-          	v :: (make_args_list (NonTerminal(Args, argssub)))
+            print_string "#Args v :: argssub" ; print_newline () ;
+            v :: (make_args_list (NonTerminal(Args, argssub)))
           )
       | _ -> (
-      	  report_error "illegal argument" ;
-      	  print_string "***( " ;
-      	  print_tree_node args ;
-      	  print_string ")***" ; print_newline () ;
-      	  ["error"]
+          report_error "illegal argument" ;
+          print_string "***( " ;
+          print_tree_node args ;
+          print_string ")***" ; print_newline () ;
+          ["error"]
         )
 
     (* tree -> abstract_tree list *)
