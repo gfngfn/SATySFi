82c5890b1c39cc1b1db0195fc58ec54d0d34336e
Suwa (K1Laptop03 Windows) <5546507742@mail.ecc.u-tokyo.ac.jp>
2016-12-08 01:26:08 +0900

    minor

--- subst.ml
+++ subst.ml
@@ -39,21 +39,6 @@ let eliminate (theta : t) (key : Tyvarid.t) =
     aux [] theta key
 
 
-let rec overwrite_type_struct (tystr : type_struct) (key : Tyvarid.t) (value : type_struct) =
-  let iter = (fun ty -> overwrite_type_struct ty key value) in
-  let (rng, tymain) = tystr in
-    match tymain with
-    | FuncType(dom, cod)                    -> (rng, FuncType(iter dom, iter cod))
-    | ListType(cont)                        -> (rng, ListType(iter cont))
-    | RefType(cont)                         -> (rng, RefType(iter cont))
-    | ProductType(lst)                      -> (rng, ProductType(List.map iter lst))
-    | TypeVariable(k)                       -> if Tyvarid.same k key then value else (rng, TypeVariable(k))
-    | VariantType(tyarglist, varntnm)       -> (rng, VariantType(List.map iter tyarglist, varntnm))
-    | TypeSynonym(tyarglist, tysynnm, cont) -> (rng, TypeSynonym(List.map iter tyarglist, tysynnm, iter cont))
-    | RecordType(asc)                       -> (rng, RecordType(Assoc.map_value iter asc))
-    | other                                 -> (rng, other)
-
-
 let rec apply_to_type_struct (theta : t) (tystr : type_struct) =
   let iter = apply_to_type_struct theta in
   let (rng, tymain) = tystr in
@@ -105,11 +90,26 @@ and emerge_in_list (tvid : Tyvarid.t) (tylist : type_struct list) =
           if bhd then (bhd, rnghd) else if btl then (btl, rngtl) else (false, dr)
 
 
+let rec replace_type_variable (tystr : type_struct) (key : Tyvarid.t) (value : type_struct) =
+  let iter = (fun ty -> replace_type_variable ty key value) in
+  let (rng, tymain) = tystr in
+    match tymain with
+    | FuncType(dom, cod)                    -> (rng, FuncType(iter dom, iter cod))
+    | ListType(cont)                        -> (rng, ListType(iter cont))
+    | RefType(cont)                         -> (rng, RefType(iter cont))
+    | ProductType(lst)                      -> (rng, ProductType(List.map iter lst))
+    | TypeVariable(k)                       -> if Tyvarid.same k key then value else (rng, TypeVariable(k))
+    | VariantType(tyarglist, varntnm)       -> (rng, VariantType(List.map iter tyarglist, varntnm))
+    | TypeSynonym(tyarglist, tysynnm, cont) -> (rng, TypeSynonym(List.map iter tyarglist, tysynnm, iter cont))
+    | RecordType(asc)                       -> (rng, RecordType(Assoc.map_value iter asc))
+    | other                                 -> (rng, other)
+
+
 let rec overwrite (theta : t) (key : Tyvarid.t) (value : type_struct) =
   match theta with
   | []                                      -> []
-  | (k, v) :: tail  when Tyvarid.same k key -> (key, value) :: (overwrite tail key value)
-  | (k, v) :: tail                          -> (k, (overwrite_type_struct v key value)) :: (overwrite tail key value)
+  | (k, v) :: tail -> if Tyvarid.same k key then (key, value) :: (overwrite tail key value)
+                                            else (k, (replace_type_variable v key value)) :: (overwrite tail key value)
 
 
 let overwrite_or_add (theta : t) (key : Tyvarid.t) (value : type_struct) =
