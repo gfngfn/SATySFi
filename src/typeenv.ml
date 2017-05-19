75e5dbf683d9027f42e5e23d938e53bb1c603fcd
Suwa (K1Laptop03 Windows) <5546507742@mail.ecc.u-tokyo.ac.jp>
2017-05-20 06:43:30 +0900

    minor

--- src/typeenv.ml
+++ src/typeenv.ml
@@ -36,9 +36,6 @@ type single_environment = poly_type VarMap.t
 
 type t = (ModuleID.t list) * name_to_id_map * (single_environment ModuleTree.t)
 
-(*
-let empty : t = (ModuleNameMap.empty, ModuleTree.empty VarMap.empty)
-*)
 
 let from_list (lst : (var_name * poly_type) list) =
   let vmapinit = List.fold_left (fun vmap (varnm, pty) -> VarMap.add varnm pty vmap) VarMap.empty lst in
@@ -73,39 +70,3 @@ let enter_new_module ((addr, nmtoid, mtr) : t) (mdlnm : module_name) =
 
 let leave_module ((addr, nmtoid, mtr) : t) =
   (List.rev (List.tl (List.rev addr)), nmtoid, mtr)
-
-
-(* ---- following are all for debugging --- *)
-(*
-let string_of_type_environment (tyenv : t) (msg : string) =
-  let rec iter (tyenv : t) =
-    match tyenv with
-    | []               -> ""
-    | (vn, ts) :: tail ->
-            "    #  "
-              ^ ( let len = String.length vn in if len >= 16 then vn else vn ^ (String.make (16 - len) ' ') )
-       ^ " : " ^ ((* string_of_mono_type ts *) "type") ^ "\n"
-              ^ (iter tail)
-  in
-      "    #==== " ^ msg ^ " " ^ (String.make (58 - (String.length msg)) '=') ^ "\n"
-    ^ (iter tyenv)
-    ^ "    #================================================================\n"
-
-
-let string_of_control_sequence_type (tyenv : t) =
-  let rec iter (tyenv : t) =
-    match tyenv with
-    | []               -> ""
-    | (vn, ts) :: tail ->
-        ( match String.sub vn 0 1 with
-          | "\\" ->
-              "    #  "
-                ^ ( let len = String.length vn in if len >= 16 then vn else vn ^ (String.make (16 - len) ' ') )
-                ^ " : " ^ ((* string_of_mono_type ts *) "type") ^ "\n" (* remains to be implemented *)
-          | _    -> ""
-        ) ^ (iter tail)
-  in
-      "    #================================================================\n"
-    ^ (iter tyenv)
-    ^ "    #================================================================\n"
-*)
--- src/typeenv.mli
+++ src/typeenv.mli
@@ -1,9 +1,7 @@
 open Types
 
 type t
-(*
-val empty : t
-*)
+
 val from_list : (var_name * poly_type) list -> t
 
 val add : t -> var_name -> poly_type -> t
@@ -13,9 +11,4 @@ val find : t -> (module_name list) -> var_name -> poly_type
 val enter_new_module : t -> module_name -> t
 
 val leave_module : t -> t
-(*
-val string_of_type_environment : t -> string -> string
-
-val string_of_control_sequence_type : t -> string
-*)
 
