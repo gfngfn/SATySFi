1026bcb29f55f5300f69d113e4bec28d97a3c3e9
Suwa (K1Laptop03 Windows) <5546507742@mail.ecc.u-tokyo.ac.jp>
2017-05-19 15:49:41 +0900

    develop hashTree.ml

--- src/hashTree.ml
+++ src/hashTree.ml
@@ -12,6 +12,28 @@ module Make (Key : Map.OrderedType) =
     let rec get (Node(x, imap) : 'a t) (addr : key list) =
       match addr with
       | []        -> x
-      | k :: tail -> let hshtr = InternalMap.find k imap in get hshtr tail
+      | k :: tail ->
+          let hshtr = InternalMap.find k imap in get hshtr tail
+
+
+    let rec update (Node(x, imap) : 'a t) (addr : key list) (f : 'a -> 'a) =
+      match addr with
+      | []        -> Node(f x, imap)
+      | k :: tail ->
+          let hshtr = InternalMap.find k imap in
+            Node(x, InternalMap.add k (update hshtr tail f) imap)
+
+
+    let rec search_backward (Node(x, imap) : 'a t) (addr : key list) (findf : 'a -> 'b option) =
+      match addr with
+      | []        -> findf x
+      | k :: tail ->
+          let hshtr = InternalMap.find k imap in
+          let res = search_backward hshtr tail findf in
+          begin
+            match res with
+            | None    -> findf x
+            | Some(_) -> res
+          end
 
   end
