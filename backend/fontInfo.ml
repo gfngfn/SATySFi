53a6637eee9c3321d139cc001f7696f1cc50e2a5
Suwa (K1Laptop03 Windows) <5546507742@mail.ecc.u-tokyo.ac.jp>
2017-07-07 16:15:17 +0900

    first success in getting widths of characters from .ttf file

--- backend/fontInfo.ml
+++ backend/fontInfo.ml
@@ -2,23 +2,42 @@
 open Core.Result
 
 
+type font_abbrev = string
 type file_name = string
 
+exception FontFormatBroken of Otfm.error
+exception NoGlyph of Uchar.t
+exception InvalidFontAbbrev of font_abbrev
 exception FailToLoadFontFormatOwingToSize   of file_name
 exception FailToLoadFontFormatOwingToSystem of string
 
 
-let file_name_hash_table : (string, file_name) Hashtbl.t = Hashtbl.create 32
+module FontAbbrevHashTable = Hashtbl.Make(
+  struct
+    type t = font_abbrev
+    let equal = (=)
+    let hash = Hashtbl.hash
+  end)
 
-let decoder_hash_table : (file_name, Otfm.decoder) Hashtbl.t = Hashtbl.create 32
+module FontFileNameHashTable = Hashtbl.Make(
+  struct
+    type t = file_name
+    let equal = (=)
+    let hash = Hashtbl.hash
+  end)
+
+
+let font_abbrev_hash_table : file_name FontAbbrevHashTable.t = FontAbbrevHashTable.create 32
+
+let font_file_name_hash_table : Otfm.decoder FontFileNameHashTable.t = FontFileNameHashTable.create 32
 
 
 let string_of_file (flnmin : file_name) : string =
   try
-    let ic = open_in_bin flnmin in
     let bufsize = 65536 in
     let buf : Buffer.t = Buffer.create bufsize in
     let byt : bytes = Bytes.create bufsize in
+    let ic : in_channel = open_in_bin flnmin in
       try
         begin
           while true do
@@ -36,16 +55,63 @@ let string_of_file (flnmin : file_name) : string =
   | Sys_error(msg) -> raise (FailToLoadFontFormatOwingToSystem(msg))
 
 
-let get_decoder (flnmin : file_name) : Otfm.decoder =
-  try
-    Hashtbl.find decoder_hash_table flnmin
-  with
-  | Not_found ->
-      let s = string_of_file flnmin in
-      let dcdr = Otfm.decoder (`String(s)) in
-      begin
-        Hashtbl.add decoder_hash_table flnmin dcdr ;
-        dcdr
-      end
+let get_decoder (fntabrv : font_abbrev) : Otfm.decoder =
+  let flnmin = 
+    try
+      FontAbbrevHashTable.find font_abbrev_hash_table fntabrv
+    with
+    | Not_found -> raise (InvalidFontAbbrev(fntabrv))
+  in
+    try
+      FontFileNameHashTable.find font_file_name_hash_table flnmin
+    with
+    | Not_found ->
+        let s = string_of_file flnmin in
+        let dcdr = Otfm.decoder (`String(s)) in
+        begin
+          FontFileNameHashTable.add font_file_name_hash_table flnmin dcdr ;
+          dcdr
+        end
 
 
+let get_glyph_id (dcdr : Otfm.decoder) (uch : Uchar.t) : Otfm.glyph_id =
+  let cp = Uchar.to_int uch in
+  let cmapres =
+    Otfm.cmap dcdr (fun accopt mapkd (u0, u1) gid ->
+      match accopt with
+      | Some(_) -> accopt
+      | None    -> if u0 <= cp && cp <= u1 then Some(gid + (cp - u0)) else None
+    ) None
+  in
+    match cmapres with
+    | Error(e)                   -> raise (FontFormatBroken(e))
+    | Ok(((_, _, _), None))      -> raise (NoGlyph(uch))
+    | Ok(((_, _, _), Some(gid))) -> gid
+
+
+let get_uchar_width (dcdr : Otfm.decoder) (uch : Uchar.t) =
+  let gidkey = get_glyph_id dcdr uch in
+  let hmtxres =
+    Otfm.hmtx dcdr (fun accopt gid adv lsb ->
+      match accopt with
+      | Some(_) -> accopt
+      | None    -> if gid = gidkey then Some(adv) else None
+    ) None
+  in
+    match hmtxres with
+    | Error(e)      -> raise (FontFormatBroken(e))
+    | Ok(None)      -> assert false
+    | Ok(Some(adv)) -> adv
+
+
+let get_width_of_word (fntabrv : font_abbrev) (uword : Uchar.t list) =
+  let dcdr = get_decoder fntabrv in
+  let chwidlst = List.map (fun uch -> get_uchar_width dcdr uch) uword in
+(* List.fold_left (+) 0 chwidlst *)
+    chwidlst (* temporary *)
+
+
+let () =
+  FontAbbrevHashTable.add font_abbrev_hash_table "Hlv" "HelveticaBlack.ttf" ;
+  let res = get_width_of_word "Hlv" (List.map Uchar.of_char ['O'; 'C'; 'a'; 'm'; 'l']) in
+    res |> List.iter (fun adv -> print_endline (string_of_int adv))
