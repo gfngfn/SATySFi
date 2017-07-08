62a140b3dd5ec89e9e6bac90d1a5841539ffa693
Suwa (K1Laptop03 Windows) <5546507742@mail.ecc.u-tokyo.ac.jp>
2017-07-08 19:36:42 +0900

    make fontInfo.ml read `glyf` table

--- backend/fontInfo.ml
+++ backend/fontInfo.ml
@@ -89,19 +89,23 @@ let get_glyph_id (dcdr : Otfm.decoder) (uch : Uchar.t) : Otfm.glyph_id =
     | Ok(((_, _, _), Some(gid))) -> gid
 
 
-let get_uchar_width (dcdr : Otfm.decoder) (uch : Uchar.t) =
+let get_uchar_horz_metrics (dcdr : Otfm.decoder) (uch : Uchar.t) =
   let gidkey = get_glyph_id dcdr uch in
   let hmtxres =
     Otfm.hmtx dcdr (fun accopt gid adv lsb ->
       match accopt with
       | Some(_) -> accopt
-      | None    -> if gid = gidkey then Some(adv) else None
+      | None    -> if gid = gidkey then Some((adv, lsb)) else None
     ) None
   in
     match hmtxres with
-    | Error(e)      -> raise (FontFormatBroken(e))
-    | Ok(None)      -> assert false
-    | Ok(Some(adv)) -> adv
+    | Error(e)             -> raise (FontFormatBroken(e))
+    | Ok(None)             -> assert false
+    | Ok(Some((adv, lsb))) -> (adv, lsb)
+
+
+let get_uchar_width (dcdr : Otfm.decoder) (uch : Uchar.t) =
+  let (adv, _) = get_uchar_horz_metrics dcdr uch in adv
 
 
 let get_width_of_word (fntabrv : font_abbrev) (uword : Uchar.t list) =
@@ -111,7 +115,74 @@ let get_width_of_word (fntabrv : font_abbrev) (uword : Uchar.t list) =
     chwidlst (* temporary *)
 
 
+let get_contour_list (dcdr : Otfm.decoder) (uch : Uchar.t) =
+  let gid = get_glyph_id dcdr uch in
+  let gloc =
+    match Otfm.loca dcdr gid with
+    | Error(e)    -> raise (FontFormatBroken(e))
+    | Ok(None)    -> raise (NoGlyph(uch))
+    | Ok(Some(l)) -> l
+  in
+    match Otfm.glyf dcdr gloc with
+    | Error(e)                     -> raise (FontFormatBroken(e))
+    | Ok((`Composite(_), _))       -> raise (NoGlyph(uch)) (* temporary; does not deal with composite glyphs *)
+    | Ok((`Simple(cntrlst), bbox)) -> (cntrlst, bbox)
+
+
+let svg_of_uchar ((xcur, ycur) : int * int) (dcdr : Otfm.decoder) (uch : Uchar.t) =
+  let (cntrlst, (xmin, ymin, xmax, ymax)) = get_contour_list dcdr uch in
+  let (adv, lsb) = get_uchar_horz_metrics dcdr uch in
+  let ( ~$ ) = string_of_int in
+  let dpoffset = 50 in
+  let display_x x = x in
+  let display_y y = 1000 - y in
+  let path_string_of_contour cntr =
+    let isfirst = ref true in
+    let lst =
+      cntr |> List.map (fun (oncurve, xpos, ypos) ->
+        let prefix =
+          if !isfirst then begin isfirst := false ; "M" end else "L"
+        in
+        let circ =
+          "<circle cx=\"" ^ (~$ (display_x xpos)) ^ "\" cy=\"" ^ (~$ (display_y ypos)) ^ "\" r=\"5\" fill=\"" ^
+            (if oncurve then "green" else "orange") ^ "\" />"
+        in
+          (prefix ^ (~$ (display_x xpos)) ^ "," ^ (~$ (display_y ypos)), circ)
+      )
+    in
+    let strlst = List.map (fun (x, _) -> x) lst in
+    let circlst = List.map (fun (_, y) -> y) lst in
+      ("<path d=\"" ^ (String.concat " " strlst) ^ " Z\" fill=\"none\" stroke=\"red\" stroke-width=\"5\" />", String.concat "" circlst)
+  in
+  let newpos = (xcur + xmax - xmin, ycur + ymax - ymin) in
+  let pclst = (cntrlst |> List.map path_string_of_contour) in
+  let pathlst = List.map (fun (x, _) -> x) pclst in
+  let circlst = List.map (fun (_, y) -> y) pclst in
+  let lst =
+    List.concat [
+      [
+        "<?xml version=\"1.0\" encoding=\"utf-8\"?>";
+        "<!DOCTYPE svg PUBLIC \"-//W3C//DTD SVG 1.1//EN\" \"http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd\">";
+        "<svg xmlns=\"http://www.w3.org/2000/svg\" xmlns:xlink=\"http://www.w3.org/1999/xlink\" width=\"1000\" height=\"1100\" viewBox=\"" ^ (~$ (display_x (0 - dpoffset))) ^ " " ^ (~$ (display_y (ymax + dpoffset))) ^ " " ^ (~$ (adv + 2 * dpoffset)) ^ " " ^ (~$ (ymax - ymin + 2 * dpoffset)) ^ "\">";
+        "<rect x=\"" ^ (~$ (display_x 0)) ^ "\" y=\"" ^ (~$ (display_y ymax)) ^ "\" width=\"" ^ (~$ adv) ^ "\" height=\"" ^ (~$ (ymax - ymin)) ^ "\" fill=\"gray\" stroke=\"purple\" stroke-width=\"5\" />";
+        "<rect x=\"" ^ (~$ (display_x xmin)) ^ "\" y=\"" ^ (~$ (display_y ymax)) ^ "\" width=\"" ^ (~$ (xmax - xmin)) ^ "\" height=\"" ^ (~$ (ymax - ymin)) ^ "\" fill=\"none\" stroke=\"blue\" stroke-width=\"5\" />";
+      ];
+      pathlst;
+      circlst;
+      ["</svg>"];
+    ]
+  in
+    (String.concat "" lst, newpos)
+
+
 let () =
   FontAbbrevHashTable.add font_abbrev_hash_table "Hlv" "HelveticaBlack.ttf" ;
-  let res = get_width_of_word "Hlv" (List.map Uchar.of_char ['O'; 'C'; 'a'; 'm'; 'l']) in
+(*
+  let testword = List.map Uchar.of_char ['O'; 'C'; 'a'; 'm'; 'l'] in
+  let res = get_width_of_word "Hlv" testword in
     res |> List.iter (fun adv -> print_endline (string_of_int adv))
+*)
+  let dcdr = get_decoder "Hlv" in
+  let (paths, _) = svg_of_uchar (100, 100) dcdr (Uchar.of_char 'R') in
+    print_endline paths
+
