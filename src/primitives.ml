9facd6f0df332a445a6f85d454129d73b8bf6e0f
gfngfn <suwa-takashi141@g.ecc.u-tokyo.ac.jp>
2017-09-15 20:30:03 +0900

    minor change in 'primitives.ml'

--- src/primitives.ml
+++ src/primitives.ml
@@ -2,7 +2,7 @@ open Types
 
 
 let add_default_types tyenvmid =
-  let dr = Range.dummy "make_variant_environment" in
+  let dr = Range.dummy "add_default_types" in
   let mutvarntcons =
     UTMutualVariantCons(
       [(dr, "%a", MUniversalKind)],  Range.dummy "primitives-maybe", "maybe",
@@ -43,15 +43,15 @@ let rec lambda3 astf env =
 
 let make_environments () =
 
-  let i             = (Range.dummy "int", IntType) in
-  let b             = (Range.dummy "bool", BoolType) in
-  let s             = (Range.dummy "string", StringType) in
-  let (~@) n        = (Range.dummy "tv", TypeVariable(n)) in
+  let i             = (Range.dummy "int"   , IntType           ) in
+  let b             = (Range.dummy "bool"  , BoolType          ) in
+  let s             = (Range.dummy "string", StringType        ) in
+  let (~@) n        = (Range.dummy "tv"    , TypeVariable(n)   ) in
   let (-%) n ptysub = ptysub in
   let (~%) ty       = Poly(ty) in
-  let l cont        = (Range.dummy "list", ListType(cont)) in
-  let r cont        = (Range.dummy "ref", RefType(cont)) in
-  let (-->) dom cod = (Range.dummy "func", FuncType(dom, cod)) in
+  let l cont        = (Range.dummy "list"  , ListType(cont)    ) in
+  let r cont        = (Range.dummy "ref"   , RefType(cont)     ) in
+  let (-->) dom cod = (Range.dummy "func"  , FuncType(dom, cod)) in
   let tv1 = (let bid1 = BoundID.fresh UniversalKind () in ref (Bound(bid1))) in
   let tv2 = (let bid2 = BoundID.fresh UniversalKind () in ref (Bound(bid2))) in
 
@@ -86,23 +86,23 @@ let make_environments () =
     let ptyderef = tv1 -% (~% ((r (~@ tv1)) --> (~@ tv1))) in
     let ptycons  = tv2 -% (~% ((~@ tv2) --> ((l (~@ tv2)) --> (l (~@ tv2))))) in
     let astfdeeper = lambda1 (fun vstr -> Concat(DeeperIndent(Concat(SoftBreakAndIndent, vstr)), SoftBreakAndIndent)) in
-      [ ( "+"  , ~% (i --> (i --> i)), loc_plus       , lambda2 (fun v1 v2 -> Plus(v1, v2)) );
-        ( "-"  , ~% (i --> (i --> i)), loc_minus      , lambda2 (fun v1 v2 -> Minus(v1, v2)) );
-        ( "mod", ~% (i --> (i --> i)), loc_mod        , lambda2 (fun v1 v2 -> Mod(v1, v2)) );
-        ( "*"  , ~% (i --> (i --> i)), loc_times      , lambda2 (fun v1 v2 -> Times(v1,v2)) );
-        ( "/"  , ~% (i --> (i --> i)), loc_divides    , lambda2 (fun v1 v2 -> Divides(v1,v2)) );
-        ( "^"  , ~% (s --> (s --> s)), loc_concat     , lambda2 (fun v1 v2 -> Concat(v1,v2)) );
-        ( "==" , ~% (i --> (i --> b)), loc_equalto    , lambda2 (fun v1 v2 -> EqualTo(v1,v2)) );
-        ( "<>" , ~% (i --> (i --> b)), loc_neq        , lambda2 (fun v1 v2 -> LogicalNot(EqualTo(v1, v2))) );
-        ( ">"  , ~% (i --> (i --> b)), loc_greaterthan, lambda2 (fun v1 v2 -> GreaterThan(v1, v2)) );
-        ( "<"  , ~% (i --> (i --> b)), loc_lessthan   , lambda2 (fun v1 v2 -> LessThan(v1, v2)) );
-        ( ">=" , ~% (i --> (i --> b)), loc_geq        , lambda2 (fun v1 v2 -> LogicalNot(LessThan(v1, v2))) );
+      [ ( "+"  , ~% (i --> (i --> i)), loc_plus       , lambda2 (fun v1 v2 -> Plus(v1, v2))                    );
+        ( "-"  , ~% (i --> (i --> i)), loc_minus      , lambda2 (fun v1 v2 -> Minus(v1, v2))                   );
+        ( "mod", ~% (i --> (i --> i)), loc_mod        , lambda2 (fun v1 v2 -> Mod(v1, v2))                     );
+        ( "*"  , ~% (i --> (i --> i)), loc_times      , lambda2 (fun v1 v2 -> Times(v1, v2))                   );
+        ( "/"  , ~% (i --> (i --> i)), loc_divides    , lambda2 (fun v1 v2 -> Divides(v1, v2))                 );
+        ( "^"  , ~% (s --> (s --> s)), loc_concat     , lambda2 (fun v1 v2 -> Concat(v1, v2))                  );
+        ( "==" , ~% (i --> (i --> b)), loc_equalto    , lambda2 (fun v1 v2 -> EqualTo(v1, v2))                 );
+        ( "<>" , ~% (i --> (i --> b)), loc_neq        , lambda2 (fun v1 v2 -> LogicalNot(EqualTo(v1, v2)))     );
+        ( ">"  , ~% (i --> (i --> b)), loc_greaterthan, lambda2 (fun v1 v2 -> GreaterThan(v1, v2))             );
+        ( "<"  , ~% (i --> (i --> b)), loc_lessthan   , lambda2 (fun v1 v2 -> LessThan(v1, v2))                );
+        ( ">=" , ~% (i --> (i --> b)), loc_geq        , lambda2 (fun v1 v2 -> LogicalNot(LessThan(v1, v2)))    );
         ( "<=" , ~% (i --> (i --> b)), loc_leq        , lambda2 (fun v1 v2 -> LogicalNot(GreaterThan(v1, v2))) );
-        ( "&&" , ~% (b --> (b --> b)), loc_land       , lambda2 (fun v1 v2 -> LogicalAnd(v1, v2)) );
-        ( "||" , ~% (b --> (b --> b)), loc_lor        , lambda2 (fun v1 v2 -> LogicalOr(v1, v2)) );
-        ( "not", ~% (b --> b)        , loc_lnot       , lambda1 (fun v1 -> LogicalNot(v1)) );
-        ( "!"  , ptyderef            , loc_deref      , lambda1 (fun v1 -> Reference(v1)) );
-        ( "::" , ptycons             , loc_cons       , lambda2 (fun v1 v2 -> ListCons(v1, v2)) );
+        ( "&&" , ~% (b --> (b --> b)), loc_land       , lambda2 (fun v1 v2 -> LogicalAnd(v1, v2))              );
+        ( "||" , ~% (b --> (b --> b)), loc_lor        , lambda2 (fun v1 v2 -> LogicalOr(v1, v2))               );
+        ( "not", ~% (b --> b)        , loc_lnot       , lambda1 (fun v1 -> LogicalNot(v1))                     );
+        ( "!"  , ptyderef            , loc_deref      , lambda1 (fun v1 -> Reference(v1))                      );
+        ( "::" , ptycons             , loc_cons       , lambda2 (fun v1 v2 -> ListCons(v1, v2))                );
 
         ( "same"         , ~% (s --> (s --> b))        , loc_same        , lambda2 (fun v1 v2 -> PrimitiveSame(v1, v2)) );
         ( "string-sub"   , ~% (s --> (i --> (i --> s))), loc_stringsub   , lambda3 (fun vstr vpos vwid -> PrimitiveStringSub(vstr, vpos, vwid)) );
