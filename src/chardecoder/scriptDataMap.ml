02cafa35f41810b2cf7d999b1388d824eb6567ba
gfngfn <suwa-takashi141@g.ecc.u-tokyo.ac.jp>
2017-10-11 01:08:40 +0900

    begin to deal with Script value

--- src/chardecoder/scriptDataMap.ml
+++ src/chardecoder/scriptDataMap.ml
@@ -1,11 +1,31 @@
 
-let script_map_ref : (string UCoreLib.UMap.t) ref = ref (UCoreLib.UMap.empty ~eq:(=))
+type script =
+  | Common              (*   --  ; Zyyy; Common *)
+  | Inherited           (*   --  ; Zinh; Inherited *)
+  | Unknown             (*   --  ; Zzzz; Unknown *)
+  | HanIdeographic      (* 'hani'; Hani; Han *)
+  | HiraganaOrKatakana  (* 'kana'; Hrkt; Hiragana_Or_Katakana *)
+  | Latin               (* 'latn'; Latn; Latin *)
+(* temporary; should add more scripts *)
+  | Other
+
+
+let read_script = function
+  | "Common"   -> Common
+  | "Han"      -> HanIdeographic
+  | "Hiragana" -> HiraganaOrKatakana
+  | "Katakana" -> HiraganaOrKatakana
+  | "Latin"    -> Latin
+  | _          -> Other
+
+
+let script_map_ref : (script UCoreLib.UMap.t) ref = ref (UCoreLib.UMap.empty ~eq:(=))
 
 
 let set_from_file filename =
   let channel = open_in filename in
   let script_list = DataParser.main DataLexer.expr (Lexing.from_channel channel) in
-  let script_map = script_list |> CharBasis.map_of_list (fun s -> s) in
+  let script_map = script_list |> CharBasis.map_of_list read_script in
   begin
     script_map_ref := script_map;
   end
