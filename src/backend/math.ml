460d1e0b4fcbb9537ece8cfa6e8851b1363cef85
gfngfn <suwa-takashi141@g.ecc.u-tokyo.ac.jp>
2017-11-21 19:08:42 +0900

    develop math

--- src/backend/math.ml
+++ src/backend/math.ml
@@ -20,7 +20,7 @@ type right_kern =
     kernBR             : FontFormat.math_kern option;
   }
 
-type low_math_element = math_kind * low_math_element_main
+type low_math_element = math_kind * low_math_element_main * left_kern * right_kern
 
 type low_math_main =
   | LowMathPure        of low_math_element
@@ -37,7 +37,7 @@ let no_left_kern = { kernTL = None; kernBL = None; }
 let no_right_kern = { italics_correction = None; kernTR = None; kernBR = None; }
 
 
-let get_left_and_right_kern micopt mkiopt : left_kern * right_kern =
+let make_left_and_right_kern micopt mkiopt : left_kern * right_kern =
   let lk =
     match mkiopt with
     | Some(mki) ->
@@ -62,55 +62,37 @@ let get_left_and_right_kern micopt mkiopt : left_kern * right_kern =
     (lk, rk)
 
 
-let convert_math_element (scriptlev : int) ((mk, memain) : math_element) : low_math_element * left_kern * right_kern =
+let convert_math_element (scriptlev : int) ((mk, memain) : math_element) : low_math_element =
   match memain with
-  | MathGraphics(g)         -> ((mk, LowMathGraphics(g)), no_left_kern, no_right_kern)
-  | MathEmbeddedHorz(hblst) -> ((mk, LowMathEmbeddedHorz(hblst)), no_left_kern, no_right_kern)
+  | MathGraphics(g)         -> (mk, LowMathGraphics(g), no_left_kern, no_right_kern)
+  | MathEmbeddedHorz(hblst) -> (mk, LowMathEmbeddedHorz(hblst), no_left_kern, no_right_kern)
   | MathChar(mathinfo, uch) ->
       let (gid, wid, hgt, dpt, micopt, mkiopt) = FontInfo.get_math_char_info mathinfo uch in
-      let (lk, rk) = get_left_and_right_kern micopt mkiopt in
-        ((mk, LowMathGlyph(mathinfo, gid)), lk, rk)
+        (* temporary; choosing glyph ID should depends on the script level. *)
+      let (lk, rk) = make_left_and_right_kern micopt mkiopt in
+        (mk, LowMathGlyph(mathinfo, gid), lk, rk)
 
-(*
-let convert_math_element_list (scriptlev : int) (melst : math_element list)
-    : (correction_info * low_math_element list) =
-  let lmelst = melst |> List.map convert_math_element in
-  let (micopt, kernoptTR, kernoptBR) =
-    match List.rev lmelst with
-    | []                                     -> (None, None, None)
-    | (_, _, micoptlast, None) :: _          -> (micoptlast, None, None)
-    | (_, _, micoptlast, Some(mkilast)) :: _ -> (micoptlast, Some(mkilast.FontFormat.kernTR), Some(mkilast.FontFormat.kernBR))
-  in
-  let (kernoptTL, kernoptBL) =
-    match lmelst with
-    | []                             -> (None, None)
-    | (_, _, _, None) :: _           -> (None, None)
-    | (_, _, _, Some(mkifirst)) :: _ -> (Some(mkifirst.FontFormat.kernTL), Some(mkifirst.FontFormat.kernBL))
-  in
-  let corrinfo =
-    {
-      italics_correction = micopt;
-      kernTL             = kernoptTL;
-      kernBL             = kernoptBL;
-      kernTR             = kernoptTR;
-      kernBR             = kernoptBR;
-    }
-  in
-    (corrinfo, lmelst)
-*)
 
-(*
-let update_left_kern lkernopt kernoptTL kernoptBL =
-  match lkernopt with
-  | None    -> Some({ kernTL = kernoptTL; kernBL = kernoptBL; })
-  | Some(_) -> lkernopt
-*)
+let get_right_kern lmmain =
+  match lmmain with
+  | LowMathPure(_, _, _, rk) -> rk
+  | _                        -> no_right_kern
+
+
+let get_left_kern lmmain =
+  match lmmain with
+  | LowMathPure(_, _, lk, _)          -> lk
+  | LowMathSubscript((_, lk, _), _)   -> lk
+  | LowMathSuperscript((_, lk, _), _) -> lk
+  | _                                 -> no_left_kern
 
 
 let rec convert (scriptlev : int) (mlst : math list) : low_math =
   let optres =
     mlst |> List.fold_left (fun opt math ->
-      let (lmmain, lk, rk) = convert_single scriptlev math in
+      let lmmain = convert_single scriptlev math in
+      let rk = get_right_kern lmmain in
+      let lk = get_left_kern lmmain in
       match opt with
       | None                      -> Some((lk, rk, lmmain :: []))
       | Some((lkfirst, _, lmacc)) -> Some((lkfirst, rk, lmmain :: lmacc))
@@ -121,29 +103,27 @@ let rec convert (scriptlev : int) (mlst : math list) : low_math =
   | Some((lkfirst, rklast, lmmainacc)) -> (List.rev lmmainacc, lkfirst, rklast)
 
 
-and convert_single (scriptlev : int) (math : math) =
+and convert_single (scriptlev : int) (math : math) : low_math_main =
   match math with
   | MathPure(me) ->
-      let ((mk, lme), lk, rk) = convert_math_element scriptlev me in
-      (LowMathPure(mk, lme), lk, rk)
+      let (mk, lme, lk, rk) = convert_math_element scriptlev me in
+        LowMathPure(mk, lme, lk, rk)
 
   | MathFraction(mlstN, mlstD) ->
       let lmN = convert scriptlev mlstN in
       let lmD = convert scriptlev mlstD in
-        (LowMathFraction(lmN, lmD), no_left_kern, no_right_kern)
+        LowMathFraction(lmN, lmD)
 
   | MathSubscript(mlst1, mlst2) ->
       let lmB = convert scriptlev mlst1 in
-      let (_, lkB, _) = lmB in
       let lmS = convert (scriptlev + 1) mlst2 in
-        (LowMathSubscript(lmB, lmS), lkB, no_right_kern)
+        LowMathSubscript(lmB, lmS)
 
   | MathSuperscript(mlst1, mlst2) ->
       let lmB = convert scriptlev mlst1 in
-      let (_, lkB, _) = lmB in
       let lmS = convert (scriptlev + 1) mlst2 in
-        (LowMathSuperscript(lmB, lmS), lkB, no_right_kern)
+        LowMathSuperscript(lmB, lmS)
 
   | MathRadical(mlst1) ->
       let lm1 = convert scriptlev mlst1 in
-        (LowMathRadical(lm1), no_left_kern, no_right_kern)
+        LowMathRadical(lm1)
