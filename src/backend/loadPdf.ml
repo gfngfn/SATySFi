eb089000073c205895eeddfddd1f253f2c7342b6
gfngfn <suwa-takashi141@g.ecc.u-tokyo.ac.jp>
2017-12-27 22:53:49 +0900

    change how to load resource dictionaries

--- src/backend/loadPdf.ml
+++ src/backend/loadPdf.ml
@@ -1,7 +1,32 @@
 
+exception PdfBroken
+
+
 let xobject_of_page (pdf : Pdf.t) (page : Pdfpage.t) : Pdf.pdfobject =
   let content = page.Pdfpage.content in
-  let pdfdict_resources = page.Pdfpage.resources in
+  let pdfdict_resources =
+    match page.Pdfpage.resources with
+    | Pdf.Dictionary(keyval) ->
+        let keyvalnew =
+          keyval |> List.map (fun pair ->
+            let (key, value) = pair in
+              match value with
+              | Pdf.Indirect(ir) ->
+                  begin
+                    try
+                      let direct = Pdf.lookup_obj pdf ir in
+                        (key, direct)
+                    with
+                    | Not_found -> raise PdfBroken
+                  end
+
+              | _ -> pair
+          )
+        in
+          Pdf.Dictionary(keyvalnew)
+
+    | _ -> raise PdfBroken
+  in
   let pdfarr_rect = page.Pdfpage.mediabox in
     (* -- uses only /MediaBox entry -- *)
   let entriesadded =
@@ -29,10 +54,10 @@ let xobject_of_page (pdf : Pdf.t) (page : Pdfpage.t) : Pdf.pdfobject =
 
 let make_xobject (pdf : Pdf.t) (pageno : int) : Pdf.pdfobject option =
   let pagelst = Pdfpage.pages_of_pagetree pdf in
-  match List.nth_opt pagelst pageno with
-  | None ->
-      None
+    match List.nth_opt pagelst pageno with
+    | None ->
+        None
 
-  | Some(page) ->
-      let pdfstream = xobject_of_page pdf page in
-      Some(pdfstream)
+    | Some(page) ->
+        let pdfstream = xobject_of_page pdf page in
+        Some(pdfstream)
