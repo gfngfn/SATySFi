f331e28a9646a449ccad172813153ca0dd53de9d
Masaki Hara <ackie.h.gmai@gmail.com>
2018-03-07 17:56:18 +0900

    Guard thread local storage by mutex.
    Currently, ParserContext is implemented via a thread local storage, but
    it has not been thread safe.
    
    The bug is not really effective, as the parser runs in a single thread.
    We can even use global storage for parser contexts. However, my feeling
    is that it is a bit creepy to leave global storage that can trivially be
    made thread-safe.

--- src/frontend/parserContext.ml
+++ src/frontend/parserContext.ml
@@ -7,19 +7,28 @@
  * Instead, we use a thread local storage to accomplish the task.
  *)
 
+let contexts_mutex = Mutex.create ()
+
+let with_locked callback =
+  Mutex.lock contexts_mutex;
+  try
+    let result = callback (Thread.id (Thread.self ())) in
+    Mutex.unlock contexts_mutex;
+    result
+  with e ->
+    Mutex.unlock contexts_mutex;
+    raise e
+
 let contexts_tls = Hashtbl.create 1
 
 let push_context ctx =
-  let tid = Thread.id (Thread.self ()) in
-  Hashtbl.add contexts_tls tid ctx
+  with_locked (fun tid -> Hashtbl.add contexts_tls tid ctx)
 
 let top_context () =
-  let tid = Thread.id (Thread.self ()) in
-  Hashtbl.find contexts_tls tid
+  with_locked (fun tid -> Hashtbl.find contexts_tls tid)
 
 let pop_context () =
-  let tid = Thread.id (Thread.self ()) in
-  Hashtbl.remove contexts_tls tid
+  with_locked (fun tid -> Hashtbl.remove contexts_tls tid)
 
 let with_context ctx callback =
   push_context ctx;
