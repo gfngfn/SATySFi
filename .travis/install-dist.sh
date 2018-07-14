5213468c9b4528cdf00d996d9b03287fac9d48de
Masaki Hara <ackie.h.gmai@gmail.com>
2018-07-14 15:48:03 +0900

    Install runtime in CI.

---
+++ .travis/install-dist.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+set -ue
+
+RUNTIME="$HOME/.satysfi"
+
+mkdir -p "$RUNTIME"
+cp -r lib-satysfi/* "$RUNTIME"/
+
+mkdir -p "$RUNTIME/dist/fonts/"
+cp \
+  /usr/share/fonts/opentype/ipaexfont-mincho/ipaexm.ttf \
+  /usr/share/fonts/opentype/ipaexfont-gothic/ipaexg.ttf \
+  /usr/share/texmf/fonts/opentype/public/lm/lmroman10-regular.otf \
+  /usr/share/texmf/fonts/opentype/public/lm/lmroman10-bold.otf \
+  /usr/share/texmf/fonts/opentype/public/lm/lmroman10-italic.otf \
+  /usr/share/texmf/fonts/opentype/public/lm/lmmono10-regular.otf \
+  /usr/share/texmf/fonts/opentype/public/lm/lmsans10-regular.otf \
+  /usr/share/fonts/truetype/junicode/Junicode.ttf \
+  /usr/share/fonts/truetype/junicode/Junicode-Bold.ttf \
+  /usr/share/fonts/truetype/junicode/Junicode-Italic.ttf \
+  /usr/share/texlive/texmf-dist/fonts/opentype/public/lm-math/latinmodern-math.otf \
+  "$RUNTIME/dist/fonts/"
+
+cp \
+  /usr/share/texlive/texmf-dist/fonts/opentype/public/Asana-Math/Asana-Math.otf \
+  "$RUNTIME/dist/fonts/Asana-math.otf"
