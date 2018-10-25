9ac7e5a09324617816b4f69a9298754ff5c0736f
Takashi Suwa <bd.gfngfn@gmail.com>
2018-10-25 21:04:02 +0900

    small refactoring and cosmetic changes on 'action.ml' and 'annotation.ml'

--- src/backend/annotation.ml
+++ src/backend/annotation.ml
@@ -8,31 +8,40 @@ type t =
   | Link of Action.t
 
 
-let annot_acc = ref []
+let annot_acc = ref Alist.empty
 
 
 let register annot rect border coloropt =
-  annot_acc := (annot, rect, border, coloropt) :: !annot_acc
+  annot_acc := Alist.extend !annot_acc (annot, rect, border, coloropt)
 
 
-let of_annotation = function
-  | (Link(act), (x, y, wid, hgt, dpt), border, coloropt) ->
-    let rect = (to_pdf_point x, to_pdf_point (y -% dpt), to_pdf_point (x +% wid), to_pdf_point (y +% hgt)) in
-    let color =
-      match coloropt with
-      | Some(DeviceRGB(r, g, b)) -> Some((int_of_float r, int_of_float g, int_of_float b))
-      | _                        -> None
-    in
-    let link = Pdfannot.make ~border:(Pdfannot.make_border (to_pdf_point border)) ~rectangle:rect Pdfannot.Link in
-      { link with
-          Pdfannot.annotrest =
-            Pdf.Dictionary ["/A", (Action.pdfobject_of_action act)];
-          Pdfannot.colour = color }
+let of_annotation (Link(act), (x, y, wid, hgt, dpt), border, coloropt) =
+  let rect = (to_pdf_point x, to_pdf_point (y -% dpt), to_pdf_point (x +% wid), to_pdf_point (y +% hgt)) in
+  let color =
+    match coloropt with
+    | Some(DeviceRGB(r, g, b)) -> Some((int_of_float r, int_of_float g, int_of_float b))
+    | _                        -> None
+  in
+  let link =
+    Pdfannot.make
+      ~border:(Pdfannot.make_border (to_pdf_point border))
+      ~rectangle:rect
+      Pdfannot.Link
+  in
+  let pdfobj_annotrest =
+    Pdf.Dictionary[("/A", Action.pdfobject_of_action act)]
+  in
+  { link with
+    Pdfannot.annotrest = pdfobj_annotrest;
+    Pdfannot.colour    = color;
+  }
 
 
 let add_to_pdf pdf page =
-  let page = List.fold_left (fun page annotinfo ->
-    Pdfannot.add_annotation pdf page (of_annotation annotinfo)) page !annot_acc in
-  let () = annot_acc := [] in
-    page
-
+  let page =
+    !annot_acc |> Alist.to_list |> List.fold_left (fun page annotinfo ->
+      Pdfannot.add_annotation pdf page (of_annotation annotinfo)
+    ) page
+  in
+  annot_acc := Alist.empty;
+  page
