c7a98d1515e64b294232d6334d95a61cf0305bfc
gfngfn <suwa-takashi141@g.ecc.u-tokyo.ac.jp>
2022-10-30 06:44:43 +0900

    add contexts to 'YamlDecoder.error'

--- src/frontend/yamlDecoder.ml
+++ src/frontend/yamlDecoder.ml
@@ -2,7 +2,14 @@
 open MyUtil
 
 
-type error =
+type context_element =
+  | Field of string
+  | Index of int
+
+type context =
+  context_element list
+
+type error_main =
   | FieldNotFound of string
   | NotAFloat
   | NotAString
@@ -11,25 +18,43 @@ type error =
   | NotAnObject
   | OtherMessage of string
 
-
-let show_error = function
-  | FieldNotFound(field) -> Printf.sprintf "field '%s' not found" field
-  | NotAFloat            -> Printf.sprintf "not a float value"
-  | NotAString           -> Printf.sprintf "not a string value"
-  | NotABool             -> Printf.sprintf "not a Boolean value"
-  | NotAnArray           -> Printf.sprintf "not an array"
-  | NotAnObject          -> Printf.sprintf "not an object"
-  | OtherMessage(msg)    -> Printf.sprintf "%s" msg
+type error =
+  context * error_main
 
 
-type 'a t = Yaml.value -> ('a, error) result
+let show_error ((context, errmain) : error) =
+  let s_main =
+    match errmain with
+    | FieldNotFound(field) -> Printf.sprintf "field '%s' not found" field
+    | NotAFloat            -> Printf.sprintf "not a float value"
+    | NotAString           -> Printf.sprintf "not a string value"
+    | NotABool             -> Printf.sprintf "not a Boolean value"
+    | NotAnArray           -> Printf.sprintf "not an array"
+    | NotAnObject          -> Printf.sprintf "not an object"
+    | OtherMessage(msg)    -> Printf.sprintf "%s" msg
+  in
+  match context with
+  | [] ->
+      s_main
+
+  | _ :: _ ->
+      let s_context =
+        context |> List.map (function
+        | Field(field) -> Printf.sprintf ".%s" field
+        | Index(index) -> Printf.sprintf ".[%d]" index
+        ) |> String.concat ""
+      in
+      Printf.sprintf "%s (context: %s)" s_main s_context
+
+
+type 'a t = context_element Alist.t * Yaml.value -> ('a, error) result
 
 
 let run (d : 'a t) (s : string) : ('a, error) result =
   let open ResultMonad in
   match Yaml.of_string s with
-  | Ok(yval)       -> d yval
-  | Error(`Msg(s)) -> err (OtherMessage(s))
+  | Ok(yval)       -> d (Alist.empty, yval)
+  | Error(`Msg(s)) -> err ([], OtherMessage(s))
 
 
 let succeed (a : 'a) : 'a t =
@@ -37,40 +62,40 @@ let succeed (a : 'a) : 'a t =
 
 
 let failure (msg : string) : 'a t =
-  fun _ -> Error(OtherMessage(msg))
+  fun (context, _) -> Error(Alist.to_list context, OtherMessage(msg))
 
 
 let bind (d : 'a t) (df : 'a -> 'b t) : 'b t =
-fun yval ->
-  match d yval with
-  | Ok(a)         -> df a yval
-  | Error(_) as e -> e
+  fun s ->
+    match d s with
+    | Ok(a)         -> df a s
+    | Error(_) as e -> e
 
 
 let ( >>= ) = bind
 
 
-let get_scheme (field : string) (d : 'a t) (k_absent : unit -> ('a, error) result) : 'a t =
-  let open ResultMonad in
-  function
-  | `O(keyvals) ->
-      begin
-        match
-          keyvals |> List.find_map (fun (k, v) ->
-            if String.equal k field then Some(v) else None
-          )
-        with
-        | None    -> k_absent ()
-        | Some(v) -> d v
-      end
+let get_scheme (field : string) (d : 'a t) (k_absent : context_element Alist.t -> ('a, error) result) : 'a t =
+  fun (context, yval) ->
+    let open ResultMonad in
+    match yval with
+    | `O(keyvals) ->
+        begin
+          match
+            keyvals |> List.find_opt (fun (k, _v) -> String.equal k field)
+              (* According to the specification of YAML, every key can occur at most once here. *)
+          with
+          | None         -> k_absent context
+          | Some((k, v)) -> d (Alist.extend context (Field(k)), v)
+        end
 
-  | _ ->
-      err NotAnObject
+    | _ ->
+        err (Alist.to_list context, NotAnObject)
 
 
 let get (field : string) (d : 'a t) : 'a t =
   let open ResultMonad in
-  get_scheme field d (fun () -> err (FieldNotFound(field)))
+  get_scheme field d (fun context -> err (Alist.to_list context, FieldNotFound(field)))
 
 
 let get_opt (field : string) (d : 'a t) : ('a option) t =
@@ -78,48 +103,51 @@ let get_opt (field : string) (d : 'a t) : ('a option) t =
     d >>= fun v -> succeed (Some(v))
   in
   let open ResultMonad in
-  get_scheme field d_some (fun () -> return None)
+  get_scheme field d_some (fun _ -> return None)
 
 
 let get_or_else (field : string) (d : 'a t) (default : 'a) : 'a t =
   let open ResultMonad in
-  get_scheme field d (fun () -> return default)
+  get_scheme field d (fun _ -> return default)
 
 
 let number : float t =
-  let open ResultMonad in
-  function
-  | `Float(x) -> return x
-  | _         -> err NotAFloat
+  fun (context, yval) ->
+    let open ResultMonad in
+    match yval with
+    | `Float(x) -> return x
+    | _         -> err (Alist.to_list context, NotAFloat)
 
 
 let string : string t =
-  let open ResultMonad in
-  function
-  | `String(x) -> return x
-  | _          -> err NotAString
+  fun (context, yval) ->
+    let open ResultMonad in
+    match yval with
+    | `String(x) -> return x
+    | _          -> err (Alist.to_list context, NotAString)
 
 
 let bool : bool t =
-  let open ResultMonad in
-  function
-  | `Bool(x) -> return x
-  | _        -> err NotABool
+  fun (context, yval) ->
+    let open ResultMonad in
+    match yval with
+    | `Bool(x) -> return x
+    | _        -> err (Alist.to_list context, NotABool)
 
 
 let list (d : 'a t) : ('a list) t =
-  let open ResultMonad in
-  function
-  | `A(yvals) ->
-      yvals |> List.fold_left (fun res yval ->
-        res >>= fun acc ->
-        d yval >>= fun a ->
-        return (Alist.extend acc a)
-      ) (return Alist.empty) >>= fun acc ->
-      return (Alist.to_list acc)
+  fun (context, yval) ->
+    let open ResultMonad in
+    match yval with
+    | `A(yvals) ->
+        yvals |> foldM (fun (index, acc) yval ->
+          d (Alist.extend context (Index(index)), yval) >>= fun a ->
+          return (index + 1, Alist.extend acc a)
+        ) (0, Alist.empty) >>= fun (_, acc) ->
+        return (Alist.to_list acc)
 
-  | _ ->
-      err NotAnArray
+    | _ ->
+        err (Alist.to_list context, NotAnArray)
 
 
 type 'a branch = string * 'a t
