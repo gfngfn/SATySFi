7943569c0b14668354400a063ff24a174f10ea27
gfngfn <suwa-takashi141@g.ecc.u-tokyo.ac.jp>
2022-11-05 04:04:30 +0900

    make lock configs support 'local'

--- src/frontend/lockConfig.ml
+++ src/frontend/lockConfig.ml
@@ -14,7 +14,7 @@ type t = {
 module LockConfigDecoder = YamlDecoder.Make(YamlError)
 
 
-let lock_location_decoder : abs_path LockConfigDecoder.t =
+let lock_location_decoder ~(lock_config_dir : abs_path) : abs_path LockConfigDecoder.t =
   let open LockConfigDecoder in
   branch "type" [
     "global" ==> begin
@@ -24,16 +24,23 @@ let lock_location_decoder : abs_path LockConfigDecoder.t =
       | Ok(abspath)       -> succeed abspath
       | Error(candidates) -> failure (fun _context -> PackageNotFound(libpath, candidates))
     end;
+    "local" ==> begin
+      get "path" string >>= fun s_relpath ->
+      let abspath =
+        make_abs_path (Filename.concat (get_abs_path_string lock_config_dir) s_relpath)
+      in
+      succeed abspath
+    end;
   ]
   ~other:(fun tag ->
     failure (fun context -> UnexpectedTag(context, tag))
   )
 
 
-let lock_decoder : lock_info LockConfigDecoder.t =
+let lock_decoder ~(lock_config_dir : abs_path) : lock_info LockConfigDecoder.t =
   let open LockConfigDecoder in
   get "name" string >>= fun lock_name ->
-  get "location" lock_location_decoder >>= fun lock_directory ->
+  get "location" (lock_location_decoder ~lock_config_dir) >>= fun lock_directory ->
   get_or_else "dependencies" (list string) [] >>= fun lock_dependencies ->
   succeed {
     lock_name;
@@ -42,9 +49,9 @@ let lock_decoder : lock_info LockConfigDecoder.t =
   }
 
 
-let lock_config_decoder : t LockConfigDecoder.t =
+let lock_config_decoder ~(lock_config_dir : abs_path) : t LockConfigDecoder.t =
   let open LockConfigDecoder in
-  get_or_else "locks" (list lock_decoder) [] >>= fun locked_packages ->
+  get_or_else "locks" (list (lock_decoder ~lock_config_dir)) [] >>= fun locked_packages ->
   succeed {
     locked_packages;
   }
@@ -60,5 +67,6 @@ let load (abspath_lock_config : abs_path) : t ok =
   in
   let s = Core.In_channel.input_all inc in
   close_in inc;
-  LockConfigDecoder.run lock_config_decoder s
+  let lock_config_dir = make_abs_path (Filename.dirname (get_abs_path_string abspath_lock_config)) in
+  LockConfigDecoder.run (lock_config_decoder ~lock_config_dir) s
     |> Result.map_error (fun e -> LockConfigError(abspath_lock_config, e))
