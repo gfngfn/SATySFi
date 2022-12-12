54c7cae02980995b6469c426a065464abf904080
gfngfn <suwa-takashi141@g.ecc.u-tokyo.ac.jp>
2022-12-12 17:58:09 +0900

    fix 'make-package-tarballs.sh'

--- make-package-tarballs.sh
+++ make-package-tarballs.sh
@@ -1,21 +1,25 @@
 #!/bin/sh
 
 TARGET_DIR=temp/tarballs
+REPO_ROOT="$(pwd)/$(dirname "$0")"
 
 echo "Deleting copied font files ..."
 for FILE in $(find lib-satysfi -name \*.ttf -or -name \*.otf | grep '^lib-satysfi/packages/'); do
-    rm "$FILE"
+    rm "${FILE}"
 done
 
-mkdir -p "$TARGET_DIR"
+mkdir -p "${TARGET_DIR}"
 for FILE in $(find lib-satysfi -name satysfi.yaml); do
-    DIR="$(dirname "$FILE")"
-    NAME="$(basename "$DIR")"
-    TARGET="$TARGET_DIR/$NAME.tar.gz"
-    echo "Compressing $DIR to $TARGET ..."
-    tar -czf "$TARGET" "$DIR"
+    VERSIONED_DIR="$(dirname "${FILE}")"
+    PACKAGE_DIR="$(dirname "${VERSIONED_DIR}")"
+    VERSIONED_NAME="$(basename "${VERSIONED_DIR}")"
+
+    ABS_TARGET="${REPO_ROOT}/${TARGET_DIR}/${VERSIONED_NAME}.tar.gz"
+
+    echo "Compressing ${VERSIONED_NAME} at ${PACKAGE_DIR} to ${ABS_TARGET} ..."
+    (cd "${PACKAGE_DIR}" && tar -czf "${ABS_TARGET}" "${VERSIONED_NAME}")
     echo "Checksum:"
-    md5sum "$TARGET"
+    md5sum "${ABS_TARGET}"
 done
 
 echo "Done."
