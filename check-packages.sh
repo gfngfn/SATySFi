9b74733b4c3f39291739d6a9939042c2e5142118
Takashi Suwa <bd.gfngfn@gmail.com>
2024-02-17 05:06:41 +0900

    slightly generalize `check-packages.sh` about the executable name

--- check-packages.sh
+++ check-packages.sh
@@ -1,17 +1,19 @@
 #!/bin/sh
 
+SAPHE="${1:-saphe}"
+
 FAILED=0
 for FILE in $(find lib-satysfi -name saphe.yaml); do
     DIR="$(dirname "$FILE")"
     echo " ==== $DIR ===="
-    saphe solve "$DIR"
+    "$SAPHE" solve "$DIR"
     if [ $? -ne 0 ]; then
         echo "! FAILED (not solved)"
         FAILED=1
     fi
     if diff "$DIR/saphe.lock.yaml" "$DIR/saphe.lock.yaml.expected"; then
         if diff "$DIR/satysfi-envelope.yaml" "$DIR/satysfi-envelope.yaml.expected"; then
-            saphe build "$DIR"
+            "$SAPHE" build "$DIR"
             if [ $? -ne 0 ]; then
                 echo "! FAILED (cannot build)"
                 FAILED=1
