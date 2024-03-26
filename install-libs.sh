2111473068e7600350cc6b36e61d131911ccca00
Takashi Suwa <bd.gfngfn@gmail.com>
2024-03-26 23:44:54 +0900

    make `install-libs.sh` do nothing

--- install-libs.sh
+++ install-libs.sh
@@ -1,14 +1,14 @@
 #!/bin/sh
 
-LIBDIR=${1:-/usr/local/share/satysfi}
-INSTALL=${2:-install}
-
-"${INSTALL}" -d "${LIBDIR}"
-"${INSTALL}" -d "${LIBDIR}/unidata"
-"${INSTALL}" -m 644 lib-satysfi/unidata/*.txt "${LIBDIR}/unidata"
-
-# It is no longer necessary to install packages beforehand due to the package system:
+# It is no longer necessary to install packages beforehand due to the package system.
+# TODO: remove this file
 #
+# LIBDIR=${1:-/usr/local/share/satysfi}
+# INSTALL=${2:-install}
+#
+# "${INSTALL}" -d "${LIBDIR}"
+# "${INSTALL}" -d "${LIBDIR}/unidata"
+# "${INSTALL}" -m 644 lib-satysfi/unidata/*.txt "${LIBDIR}/unidata"
 # "${INSTALL}" -d "${LIBDIR}/hyph"
 # "${INSTALL}" -m 644 lib-satysfi/hyph/* "${LIBDIR}/hyph"
 # "${INSTALL}" -d "${LIBDIR}/packages"
