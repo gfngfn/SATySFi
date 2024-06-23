debf8ee7c4e32799b76682fe645cbb13087a93e8
Takashi Suwa <bd.gfngfn@gmail.com>
2024-06-23 15:14:48 +0900

    update tests for external registries

--- test/saphe/packageConstraintSolverTest.ml
+++ test/saphe/packageConstraintSolverTest.ml
@@ -5,7 +5,7 @@ module Constant = SapheMain__Constant
 module PackageConstraintSolver = SapheMain__PackageConstraintSolver
 
 
-let registry_hash_value =
+let default_registry_hash_value =
   "c0bebeef4423"
 
 
@@ -17,7 +17,7 @@ let language_version =
   make_version "0.1.0"
 
 
-let make_registered_dependency ~(used_as : string) (package_name : package_name) (s_version : string) : package_dependency =
+let make_registered_dependency ~(used_as : string) ?(registry_hash_value : registry_hash_value = default_registry_hash_value) (package_name : package_name) (s_version : string) : package_dependency =
   PackageDependency{
     used_as;
     spec =
@@ -54,23 +54,23 @@ let make_impl (s_version : string) ?(registry_remotes : registry_remote list = [
   }
 
 
-let make_registered_lock (package_name : package_name) (s_version : string) : RegisteredLock.t =
+let make_registered_lock ?(registry_hash_value : registry_hash_value = default_registry_hash_value) (package_name : package_name) (s_version : string) : RegisteredLock.t =
   RegisteredLock.{
     registered_package_id = RegisteredPackageId.{ package_name; registry_hash_value };
     locked_version        = make_version s_version;
   }
 
 
-let make_locked_dependency ~(used_as : string) (package_name : package_name) (s_version : string) : locked_dependency =
+let make_locked_dependency ~(used_as : string) ?(registry_hash_value : registry_hash_value = default_registry_hash_value) (package_name : package_name) (s_version : string) : locked_dependency =
   {
-    depended_lock      = Lock.Registered(make_registered_lock package_name s_version);
+    depended_lock      = Lock.Registered(make_registered_lock ~registry_hash_value package_name s_version);
     dependency_used_as = used_as;
   }
 
 
-let make_registered_solution ?(explicitly_depended : string option) ?(test_only : bool = false) (package_name : package_name) (s_version : string) (deps : locked_dependency list) : package_solution =
+let make_registered_solution ?(explicitly_depended : string option) ?(test_only : bool = false) ?(registry_hash_value : registry_hash_value = default_registry_hash_value) (package_name : package_name) (s_version : string) (deps : locked_dependency list) : package_solution =
   {
-    lock                = Lock.Registered(make_registered_lock package_name s_version);
+    lock                = Lock.Registered(make_registered_lock ~registry_hash_value package_name s_version);
     locked_source       = NoSource;
     locked_dependencies = deps;
     used_in_test_only   = test_only;
@@ -95,11 +95,18 @@ let check package_context dependencies_with_flags expected =
   Alcotest.(check (option (list (of_pp pp_package_solution)))) "solutions" expected got
 
 
-let make_registered_package_impls =
-  List.fold_left (fun map (package_name, impls) ->
+type entry = registry_hash_value * package_name * implementation_record list
+
+
+let make_registered_package_impls : entry list -> (implementation_record list) RegisteredPackageIdMap.t =
+  List.fold_left (fun map (registry_hash_value, package_name, impls) ->
     let registered_package_id = RegisteredPackageId.{ package_name; registry_hash_value } in
     map |> RegisteredPackageIdMap.add registered_package_id impls
   ) RegisteredPackageIdMap.empty
+
+
+let make_entry ?(registry_hash_value : registry_hash_value = default_registry_hash_value) (package_name : package_name) (impls : implementation_record list) : entry =
+  (registry_hash_value, package_name, impls)
 
 
 let make_local_fixed_dependencies =
@@ -113,16 +120,16 @@ let solve_test_1 () =
     let local_fixed_dependencies = make_local_fixed_dependencies [] in
     let registered_package_impls =
       make_registered_package_impls [
-        ("foo", [
+        make_entry "foo" [
           make_impl "1.0.0" [];
           make_impl "2.0.0" [];
-        ]);
-        ("bar", [
+        ];
+        make_entry "bar" [
           make_impl "1.0.0" [ make_dependency_in_registry ~used_as:"Foo" "foo" "2.0.0" ];
-        ]);
-        ("qux", [
+        ];
+        make_entry "qux" [
           make_impl "1.0.0" [ make_dependency_in_registry ~used_as:"Foo" "foo" "1.0.0" ];
-        ]);
+        ];
       ]
     in
     { language_version; local_fixed_dependencies; registered_package_impls }
@@ -153,16 +160,16 @@ let solve_test_2 () =
     let local_fixed_dependencies = make_local_fixed_dependencies [] in
     let registered_package_impls =
       make_registered_package_impls [
-        ("foo", [
+        make_entry "foo" [
           make_impl "1.0.0" [];
           make_impl "1.1.0" [];
-        ]);
-        ("bar", [
+        ];
+        make_entry "bar" [
           make_impl "1.0.0" [ make_dependency_in_registry ~used_as:"FooA" "foo" "1.1.0" ];
-        ]);
-        ("qux", [
+        ];
+        make_entry "qux" [
           make_impl "1.0.0" [ make_dependency_in_registry ~used_as:"FooB" "foo" "1.0.0" ];
-        ]);
+        ];
       ]
     in
     { language_version; local_fixed_dependencies; registered_package_impls }
@@ -197,12 +204,12 @@ let solve_test_3 () =
     in
     let registered_package_impls =
       make_registered_package_impls [
-        ("foo", [
+        make_entry "foo" [
           make_impl "1.0.0" [];
-        ]);
-        ("bar", [
+        ];
+        make_entry "bar" [
           make_impl "1.0.0" [ make_dependency_in_registry ~used_as:"FooA" "foo" "1.0.0" ];
-        ]);
+        ];
       ]
     in
     { language_version; local_fixed_dependencies; registered_package_impls }
@@ -226,9 +233,61 @@ let solve_test_3 () =
   check package_context dependencies_with_flags expected
 
 
+let solve_test_4 () =
+  let external_registry_hash_value = "effec4f01023" in
+  let package_context =
+    let local_fixed_dependencies = make_local_fixed_dependencies [] in
+    let registered_package_impls =
+      make_registered_package_impls [
+        make_entry ~registry_hash_value:external_registry_hash_value "foo" [
+          make_impl "1.0.0" [];
+          make_impl "2.0.0" [];
+        ];
+        make_entry "bar" [
+          make_impl "1.0.0"
+            ~registry_remotes:[ GitRegistry{ url = "example.com"; branch = "master" } ] (* Dummy *)
+            [
+               make_dependency_in_registry
+                 ~used_as:"Foo" ~external_registry_hash_value "foo" "2.0.0"
+            ];
+        ];
+        make_entry "qux" [
+          make_impl "1.0.0" [ make_dependency_in_registry ~used_as:"Bar" "bar" "1.0.0" ];
+        ];
+      ]
+    in
+    { language_version; local_fixed_dependencies; registered_package_impls }
+  in
+  let dependencies_with_flags =
+    [
+      (SourceDependency, make_registered_dependency ~used_as:"Bar" "bar" "1.0.0");
+      (SourceDependency, make_registered_dependency ~used_as:"Qux" "qux" "1.0.0");
+    ]
+  in
+  let expected =
+    Some([
+      make_registered_solution ~explicitly_depended:"Bar"
+        "bar" "1.0.0" [
+          make_locked_dependency
+            ~used_as:"Foo" ~registry_hash_value:external_registry_hash_value "foo" "2.0.0";
+        ];
+      make_registered_solution ~explicitly_depended:"Qux"
+        "qux" "1.0.0" [
+          make_locked_dependency
+            ~used_as:"Bar" "bar" "1.0.0"
+        ];
+      make_registered_solution
+        ~registry_hash_value:external_registry_hash_value
+        "foo" "2.0.0" [];
+    ])
+  in
+  check package_context dependencies_with_flags expected
+
+
 let test_cases =
   Alcotest.[
-    test_case "solve 1" `Quick solve_test_1;
-    test_case "solve 2" `Quick solve_test_2;
-    test_case "solve 3" `Quick solve_test_3;
+    test_case "solve 1 (basic)" `Quick solve_test_1;
+    test_case "solve 2 (different versions of a common package)" `Quick solve_test_2;
+    test_case "solve 3 (local fixed dependencies)" `Quick solve_test_3;
+    test_case "solve 4 (external registries)" `Quick solve_test_4;
   ]
